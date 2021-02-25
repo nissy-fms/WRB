55e77a205c1b4a74d00f1da30d50fc60220c599c
nissy-fms <nissyfms@gmail.com>
2021-02-25 22:41:36 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
-function setup() {
+let capture;
 
+function setup() {
+createCanvas(640, 480);
+capture = createCapture(VIDEO);
+capture.size(640, 480);
+capture.hide();
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+background(0);
+image(capture, 0, 0, 640, 480);
+filter(POSTERIZE, 3);
 }
