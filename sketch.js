91065972609bb8d1b95fc78c8b6756fa23b97dcb
nissy-fms <nissyfms@gmail.com>
2021-04-19 16:51:37 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,8 +7,8 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    //createCanvas(563, 945);
+    //createCanvas(windowWidth, windowHeight);
+    createCanvas(563, 945);
     capture = createCapture(VIDEO);
     capture.size(640, 480);
     capture.hide();
