e9993737cbc8b677ff1560191d5094b1e0b14e94
nissy-fms <nissyfms@gmail.com>
2021-04-19 16:13:18 +0900

    Create jquery_mobile_js_link.js

---
+++ jquery_mobile_js_link.js
@@ -0,0 +1,28 @@
+// Initialization
+jQuery.mobile_js_link = {
+	init: function() {
+		for (module in jQuery.mobile_js_link) {
+			if (jQuery.mobile_js_link[module].init)
+				jQuery.mobile_js_link[module].init();
+		}
+	}
+};
+jQuery(document).ready(jQuery.mobile_js_link.init);
+jQuery.mobile_js_link.rewrite = {
+	init: function() {
+	    var ua = navigator.userAgent;
+	    if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0){}
+			else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0){}
+			else{
+	        return;
+	    }
+		var all_tags = $('a');
+		all_tags.each(function(){
+			var url = $(this).attr('href');
+			$(this).attr('href','#');
+			$(this).click(function(){
+				location.href = url;
+			});
+		});
+	}
+};
